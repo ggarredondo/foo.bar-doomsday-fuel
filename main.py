135fd9207fd84334bf0c4532939535a8a37098f9
Rullow <rullowgl@gmail.com>
2021-03-11 13:10:13 +0100

    Preparing the solution

---
+++ main.py
@@ -0,0 +1,9 @@
+import solution
+
+
+def main():
+    print(solution.solution([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
+
+
+if __name__ == '__main__':
+    main()
