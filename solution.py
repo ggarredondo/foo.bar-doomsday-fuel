1236798e641153fc1b9d80366319ce1df8b7e85e
Rullow <rullowgl@gmail.com>
2021-03-12 21:07:52 +0100

    Getting there

--- solution.py
+++ solution.py
@@ -1,4 +1,41 @@
+import numpy as np
+import fractions as f
+
+
+def standard_form(matrix):
+    rows, columns = matrix.shape
+    swaps = 0
+
+    for i in range(0, rows):
+        if not np.any(matrix[i]):
+            matrix = np.row_stack((matrix[i], matrix))
+            matrix = np.delete(matrix, i+1, 0)
+            swaps += 1
+        else:
+            matrix[i] /= matrix[i].sum()
+
+    for i in range(0, columns-swaps):
+        matrix = np.column_stack((matrix, matrix[:, 0]))
+        matrix = np.delete(matrix, 0, 1)
+
+    for i in range(0, swaps):
+        matrix[i, i] = 1
+    return matrix, swaps
 
 
 def solution(m):
-    return None
+    states, swaps = standard_form(np.matrix(m, dtype=float))
+    Q = states[swaps:, swaps:]
+    I = np.diag(np.ones(len(Q)))
+    R = states[swaps:, :swaps]
+    F = (I-Q)**(-1)
+
+    FR = F*R
+    result = map(f.Fraction, FR[0].A1.tolist())
+    denominators = []
+    for i in range(0, len(result)):
+        result[i] = result[i].limit_denominator(100)
+        denominators.append(result[i].denominator)
+    lcm = np.lcm.reduce(denominators)
+
+    return result
